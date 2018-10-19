fa8911206a177731f74491432808852683578e1d
ElenaYulova <elenavy@gmail.com>
2018-10-19 19:18:08 +0300

    errors

---
+++ Mobile/components/MobileClient.js
@@ -0,0 +1,145 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+
+import './MobileClient.css';
+
+class MobileClient extends React.PureComponent {
+
+  static propTypes = {
+    client: PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      fam: PropTypes.string.isRequired,
+      im: PropTypes.string.isRequired,
+      otch: PropTypes.string.isRequired,
+      balance: PropTypes.number.isRequired,
+      shown: PropTypes.bool.isRequired,
+      edited: PropTypes.bool,
+    }),
+       
+  };
+  static defaultProps = {
+    client: {
+      edited: false,
+    }
+  }
+  state = {
+    FIO: {
+      fam: this.props.client.fam,
+      im: this.props.client.im,
+      otch: this.props.client.otch,      
+    },
+    balance: this.props.balance,
+    edited: this.props.client.edited,
+    activeClient: this.props.client.balance >= 0,
+  };
+
+  componentWillReceiveProps = (newProps) => {
+    //console.log("MobileClient id="+this.props.id+" componentWillReceiveProps");
+    var newClient = newProps.client;
+    if (this.state.FIO.fam != newClient.fam || this.state.FIO.im != newClient.im || this.state.FIO.otch != newClient.otch) {
+      this.setState({
+        FIO: {
+          fam: newClient.fam,
+          im: newClient.im,
+          otch: newClient.otch,
+        },
+        edited: false,
+      });
+    }
+    if (this.state.balance != newClient.balance) {
+      this.setState({
+        balance: newProps.client.balance,
+        isActive: newProps.client.balance >= 0,
+      });
+    }
+  };
+  editButton = () => {
+    this.setState({ edited: true });
+  }
+  increaseBalance = () => {
+    this.props.onBalanceChange(this.props.client.id, ++this.state.balance);
+    if (this.state.balance >= 0) {
+      this.setState({
+        activeClient: true,
+      });
+    }
+  }
+
+  decreaseBalance = () => {
+    this.props.onBalanceChange(this.props.client.id, --this.state.balance);
+    if (this.state.balance < 0) {
+      this.setState({
+        activeClient: false,
+      });
+    }
+  }
+  deleteClient = () => {
+    this.props.onDelete(this.props.client.id);
+  }
+  saveForm = (EO) => {
+    EO.preventDefault();
+    var clientForm = EO.target;
+    var FIO = {
+      fam: clientForm.fam.value,
+      im: clientForm.im.value,
+      otch: clientForm.otch.value,
+    };
+    this.props.onFIOChange(this.props.client.id, FIO);
+    this.setState({
+       edited: false 
+      });
+  }
+  render() {
+
+    console.log("MobileClient id="+this.props.id+" render");
+    
+    return (
+      <div className='MobileClient'>{
+        this.props.client.shown && <div className='MobileClient'>
+            <span className="MobileClientId">{this.props.client.id}</span>
+            <div className='MobileClientFio'>
+              {
+                this.state.edited
+                  ?
+                  <form onSubmit={this.saveForm} className="MobileClientFioForm">
+                    <input type="text" defaultValue={this.state.fam} name="fam" />
+                    <input type="text" defaultValue={this.state.im} name="im" />
+                    <input type="text" defaultValue={this.state.otch} name="otch" />
+                    <input type="submit" value="Save" />
+                  </form>
+                  :
+                  <div className="MobileClientFioContainer">
+                    <span>{this.state.fam + " " + this.state.im + " " + this.state.otch}</span>
+                    <button onClick={this.editButton}>Edit</button>
+                    </div>
+              }
+              </div>
+              <div>
+              <span className="MobileClientBalance">{this.state.balance}</span>
+              <button onClick={this.decreaseBalance}>Decrease</button>
+              <button onClick={this.increaseBalance}>Increase</button>
+              </div>
+              <div className="MobileClientActive">
+              {
+                this.state.activeClient
+                  ?
+                  <span className="MobileClientActiveActive">Active</span>
+                  :
+                  <span className="MobileClientActiveBlocked">Blocked</span>
+              }
+            </div>
+            <div className="MobileClientDelete">
+              <button onClick={this.deleteClient}>Delete</button>
+            </div>
+        </div>
+      }
+       </div> 
+        
+      
+    );
+
+  }
+
+}
+
+export default MobileClient;
---
+++ Mobile/components/MobileCompany.js
@@ -0,0 +1,194 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+
+import MobileClient from './MobileClient';
+
+import './MobileCompany.css';
+
+class MobileCompany extends React.PureComponent {
+
+  static propTypes = {
+    name: PropTypes.string.isRequired,
+    clients: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        fam: PropTypes.string.isRequired,
+        im: PropTypes.string.isRequired,
+        otch: PropTypes.string.isRequired,
+        balance: PropTypes.number.isRequired,
+      })
+    ),
+  };
+
+  state = {
+    name: this.props.name,
+    clients: this.props.clients.map((v) => {
+      v.shown = true;
+      return v;
+    }),
+  };
+
+  setName = (EO) => {
+    this.setState({name:EO.target.value});
+  };
+
+ 
+  
+  setBalance = (clientId,newBalance) => {
+    let changed=false;
+    let newClients=[...this.state.clients]; // копия самого массива клиентов
+    newClients.forEach( (c,i) => {
+      if ( c.id==clientId && c.balance!=newBalance ) {
+        let newClient={...c}; // копия хэша изменившегося клиента
+        newClient.balance=newBalance;
+        newClients[i]=newClient;
+        changed=true;
+      }
+    } );
+    if ( changed )
+      this.setState({clients:newClients});
+  };
+  
+  setClientFIO  = (clientId,newFIO) => {
+    let changed=false;
+    let newClients=[...this.state.clients]; // копия самого массива клиентов
+    newClients.forEach( (c,i) => {
+      if ( c.id==clientId &&(c.fam != newFIO.fam || c.im != newFIO.im || c.otch != newFIO.otch) ) {
+        let newClient={...c}; // копия хэша изменившегося клиента
+        newClient.fam = newFIO.fam;
+        newClient.im = newFIO.im;
+        newClient.otch = newFIO.otch;
+        newClients[i]=newClient;
+        changed=true;
+      }
+    } );
+    if ( changed )
+      this.setState({clients:newClients});
+  };
+
+  addClient = () => {
+    let currentClients = this.state.clients;
+    let newClientId = currentClients[currentClients.length - 1].id + 1;
+    let newClients = [...this.state.clients];
+    let newClient = {
+      id: newClientId,
+      fam: "",
+      im: "",
+      otch: "",
+      balance: this.state.filter == "blocked" ? -1 : 0,
+      edited: true,
+      shown: true,
+    }
+    newClients.push(newClient);
+    this.setState({ clients: newClients });
+  } 
+
+  deleteClient = (clientId) => {
+    let newClients=[...this.state.clients];
+    newClients.forEach((c, i) => {
+      if (c.id == clientId) {
+        newClients.splice(i, 1);
+        return;
+      }
+    });
+    this.setState({ clients: newClients });
+  };
+  
+  filterClients = (EO) => {
+
+    let newFilter = EO.target.value;
+    if (newFilter != this.state.filter) {
+      let newClients = [...this.state.clients];
+      let changed = false;
+
+      let changedMark = (v, i, mark) => {
+        let newClient = { ...v };
+        newClient.shown = mark;
+        newClients[i] = newClient;
+        changed = true;
+      };
+
+      if (newFilter == 'all') {
+        newClients.forEach((v, i) => {
+          if (!v.shown) {
+            changedMark(v, i, true);
+          }
+        });
+      } else if (newFilter == 'blocked') {
+        newClients.forEach((v, i) => {
+          if (v.balance >= 0 && v.isShown) {
+            changedMark(v, i, false);
+          } else if (v.balance < 0 && !v.shown) {
+            changedMark(v, i, true);
+          }
+        });
+      }else if (newFilter == "active") {
+        newClients.forEach((v, i) => {
+          if (v.balance >= 0 && !v.shown) {
+            changedMark(v, i, true);
+          } else if (v.balance < 0 && v.isShown) {
+            changedMark(v, i, false);
+          }
+        });
+      } 
+      if (changed) {
+        this.setState({
+          clients: newClients,
+        });
+
+      this.setState({
+        filter: newFilter,
+      });
+
+      
+      }
+    }
+  }
+  render() {
+
+    console.log("MobileCompany render");
+
+    var clients=this.state.clients.map( client => {
+        
+        return <MobileClient 
+        key={client.id} 
+        client={client}
+        onFIOChange={this.setClientFIO}
+        onBalanceChange={this.setBalance}
+        onDelete={this.deleteClient}
+         />;
+      }
+    );
+
+    return (
+      <div className='MobileCompany'>
+         <div className="MobileCompanyTitle">
+          <div>
+            <input type="button" value="Velcom" onClick={this.setName} />
+            <input type="button" value="MTC" onClick={this.setName} />
+          </div>
+          <div className="">
+            <label htmlFor="filter">Show</label>
+            <select id="filter" value={this.state.filter} onChange={this.filterClients}>
+              <option type="radio" id="filter" value="all">All</option>
+              <option type="radio" id="filter" value="active">Active</option>
+              <option type="radio" id="filter" value="blocked">Blocked</option>
+            </select>
+          </div>
+        </div>
+        <div className='MobileCompanyName'>Компания &laquo;{this.state.name}&raquo;</div>
+        <div className='MobileCompanyClients'>
+          {clients}
+        </div>
+        <button className="MobileNewClientButton" onClick={this.addClient}>
+          Add new client
+        </button>
+      </div>
+    )
+    ;
+
+  }
+
+}
+
+export default MobileCompany;
