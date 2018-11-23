f7a0a7064f5f2db1b3b5a42012082fbc2a8c2eea
ElenaYulova <elenavy@gmail.com>
2018-11-23 21:28:17 +0300

    sprite to check

---
+++ Sprite1/Sprite1/src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { SpriteComponent } from './sprite.component';
+
+@NgModule({
+  imports: [ BrowserModule ],
+  declarations: [
+    AppComponent,
+    SpriteComponent
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
